b36bf118ac517df8cf0c683241060b101ea94dd5
Facundoegarompolo <facundoegarompolo@gmail.com>
2025-11-10 19:08:50 -0300

    Adiccion menus, login, User

--- sensorMngr/src/main/java/com/uade/tpo/SensorMngrMain.java
+++ sensorMngr/src/main/java/com/uade/tpo/SensorMngrMain.java
@@ -3,11 +3,38 @@ package com.uade.tpo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uade.tpo.mongoDB.MongoDBCRUD;
+import com.uade.tpo.views.*;
+import com.uade.tpo.entity.User;
+import org.springframework.boot.CommandLineRunner;
+
+
+
 @SpringBootApplication
-public class SensorMngrMain {
+public class SensorMngrMain implements CommandLineRunner {
+
+    private final MongoDBCRUD mongoDBCRUD;
+
+    public SensorMngrMain(MongoDBCRUD mongoDBCRUD) {
+        this.mongoDBCRUD = mongoDBCRUD;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SensorMngrMain.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
         System.out.println("SensorMngr started!");
+        ViewLogin viewLogin = new ViewLogin(mongoDBCRUD);
+        User usuario = viewLogin.mostrarMenuPrincipal();
+        if (usuario.getTipoUsuario().equals("admin")) {
+            MenuAdmin menuAdmin = new MenuAdmin(usuario);
+            menuAdmin.mostrarMenu();
+        } else if (usuario.getTipoUsuario().equals("user")) {
+            MenuUser menuUser = new MenuUser(usuario);
+            menuUser.mostrarMenu();
+        }
+
     }
 }
---
+++ sensorMngr/src/main/java/com/uade/tpo/mongoDB/MongoDBCRUD.java
@@ -0,0 +1,39 @@
+package com.uade.tpo.mongoDB;
+
+import java.util.Optional;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Component;
+
+import com.uade.tpo.entity.User;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+@Component
+public class MongoDBCRUD {
+
+    private final MongoDatabase mongoDatabase;
+
+    public MongoDBCRUD(MongoDatabase mongoDatabase) {
+        this.mongoDatabase = mongoDatabase;
+    }
+
+    public Optional<User> buscarUsuarioPorMailYPassword(String mail, String password) {
+        MongoCollection<Document> collection = mongoDatabase.getCollection("User");
+        Document filtro = new Document("mail", mail).append("password", password);
+
+        Document doc = collection.find(filtro).first();
+        if (doc == null) return Optional.empty();
+
+        String id = doc.containsKey("_id") ? ((ObjectId) doc.get("_id")).toHexString() : null;
+        String nombre = doc.getString("nombre");
+        String mailField = doc.getString("mail");
+        String pass = doc.getString("password");
+        Integer edad = doc.containsKey("edad") ? doc.getInteger("edad") : null;
+        String tipoUsuario = doc.containsKey("tipoUsuario") ? doc.getString("tipoUsuario") : "user";
+
+        User user = new User(id, nombre, mailField, pass, edad, tipoUsuario);
+        return Optional.of(user);
+    }
+}
